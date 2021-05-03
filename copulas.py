e55e67620bcb06310b159311236817655d09061a
JoeJoe1313 <levcheva_joana@yahoo.com>
2021-05-04 02:52:50 +0300

    cleaner code

--- copulas.py
+++ copulas.py
@@ -5,14 +5,14 @@ import numpy as np
 def plot_random_variable(random_variable_1, random_variable_2, random_variable_3=None):
     if random_variable_3 is None:
         _, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
-        zip_statement = zip([ax1, ax3], [ax2, ax4], [
-                            random_variable_1, random_variable_2], ['u', 't'])
+        axis1, axis2, variables, names = [ax1, ax3], [ax2, ax4], [
+            random_variable_1, random_variable_2], ['u', 't']
     elif random_variable_3 is not None:
         _, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(10, 15))
-        zip_statement = zip([ax1, ax3, ax5], [ax2, ax4, ax6], [random_variable_1,
-                                                               random_variable_2, random_variable_3], ['r', 's', 't'])
+        axis1, axis2, variables, names = [ax1, ax3, ax5], [ax2, ax4, ax6], [
+            random_variable_1, random_variable_2, random_variable_3], ['r', 's', 't']
 
-    for ax_1, ax_2, var, name in zip([ax1, ax3], [ax2, ax4], [random_variable_1, random_variable_2], ['u', 't']):
+    for ax_1, ax_2, var, name in zip(axis1, axis2, variables, names):
         ax_1.plot(var, 'ob')
         ax_1.set_title(f'Random variable {name}', size=15)
         count, bins, ignored = ax_2.hist(var, 20, density=False)
@@ -23,7 +23,7 @@ def plot_random_variable(random_variable_1, random_variable_2, random_variable_3
     plt.show()
 
 
-def plot_contour(copula_function):
+def plot_contour(copula_function, name):
     u = np.linspace(0, 1, 10)
     v = np.linspace(0, 1, 10)
     U, V = np.meshgrid(u, v)
@@ -35,13 +35,13 @@ def plot_contour(copula_function):
     ax.set_xlabel('u')
     ax.set_ylabel('v')
     ax.set_zlabel('z')
-    ax.set_title('Marshall-Olkin copula')
+    ax.set_title(f'{name} copula')
 
     plt.tight_layout()
     plt.show()
 
 
-def plot_surface(copula_function):
+def plot_surface(copula_function, name):
     u = np.linspace(0, 1, 10)
     v = np.linspace(0, 1, 10)
     U, V = np.meshgrid(u, v)
@@ -49,7 +49,7 @@ def plot_surface(copula_function):
 
     ax = plt.axes(projection='3d')
     ax.plot_surface(U, V, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='black')
-    ax.set_title('Marshall-Olkin copula')
+    ax.set_title(f'{name} copula')
 
     plt.tight_layout()
     plt.show()
@@ -81,6 +81,24 @@ def plot_amh(thetas: list, u, t):
         b = - theta * (2 * a * t + 1) + 2 * (theta ** 2) * (a ** 2) * t + 1
         c = (theta ** 2) * (4 * (a ** 2) * t - 4 * a * t + 1) - theta * (4 * a * t - 4 * t + 2) + 1
         v = (2 * t * ((a * theta - 1) ** 2)) / (b + (c ** 0.5))
+
+        ax.scatter(u, v)
+        ax.set_title(f'theta = {theta}', size=20)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_plackett(thetas: list, u, t):
+    _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30, 10))
+    axes = [ax1, ax2, ax3]
+
+    for theta, ax in zip(thetas, axes):
+        a = t * (1 - t)
+        b = theta + a * (theta - 1) ** 2
+        c = 2 * a * (u * (theta ** 2) + 1 - u) + theta * (1 - 2 * a)
+        d = (theta ** 0.5) * (theta + 4 * a * u * (1 - u) * (1 - theta) ** 2) ** 0.5
+        v = (c - (1 - 2 * t) * d) / (2 * b)
 
         ax.scatter(u, v)
         ax.set_title(f'theta = {theta}', size=20)
